6ce8926ddf3c61cbb2f3de6d3cf9a17c62818bcd
jieun song <0scarl1216@mju.ac.kr>
2025-01-19 17:16:12 +0900

    feat: camera default setting, add xml

--- app/src/main/java/com/example/moneychanger/CameraActivity.kt
+++ app/src/main/java/com/example/moneychanger/CameraActivity.kt
@@ -1,16 +1,41 @@
 package com.example.moneychanger
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.view.PreviewView
 import com.example.moneychanger.databinding.ActivityCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
+    private lateinit var previewView: PreviewView
+    private lateinit var captureButton: Button
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        previewView = binding.previewView
+        captureButton = binding.cameraButton
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        startCamera()
+
+        captureButton.setOnClickListener{
+            takePicture()
+        }
+    }
+
+    private fun takePicture() {
+        TODO("Not yet implemented")
+    }
+
+    private fun startCamera() {
+        TODO("Not yet implemented")
     }
 }
