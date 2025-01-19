e3ba3a48d9f0fa86cc3d98df89d4e0a5e95a83c3
jieun song <0scarl1216@mju.ac.kr>
2025-01-19 19:59:18 +0900

    feat: camera preview, capture, store the pic

--- app/src/main/java/com/example/moneychanger/CameraActivity.kt
+++ app/src/main/java/com/example/moneychanger/CameraActivity.kt
@@ -1,18 +1,45 @@
 package com.example.moneychanger
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.media.Image
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.view.PreviewView
-import com.example.moneychanger.databinding.ActivityCameraBinding
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import android.util.Log
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.video.FallbackStrategy
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.VideoRecordEvent
+import androidx.core.content.PermissionChecker
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
+import android.widget.Button
+import androidx.camera.view.PreviewView
+import com.example.moneychanger.databinding.ActivityCameraBinding
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
     private lateinit var previewView: PreviewView
     private lateinit var captureButton: Button
     private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +57,84 @@ class CameraActivity : AppCompatActivity() {
             takePicture()
         }
     }
-
-    private fun takePicture() {
-        TODO("Not yet implemented")
-    }
-
     private fun startCamera() {
-        TODO("Not yet implemented")
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build()
+            imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY) // 지연 최소화 설정
+                .build()
+
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            preview.surfaceProvider = previewView.surfaceProvider
+            try{
+                Log.v("CameraActivity", "startCamera.try")
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }catch (exc:Exception){
+                Log.e("CameraActivity", "Use case binding failed", exc)
+            }
+        },ContextCompat.getMainExecutor(this))
     }
+    private fun takePicture() {
+        Log.v("takePicture", "Capture button clicked!")
+        val imageCapture = imageCapture ?: return
+
+        Log.v("takePicture", "came here")
+        //ImageCapture 사용사례에 대한 참조를 불러온다. 사용사례가 null이면 함수 종료라는데,,, 몬소린지 모르겟음
+//        val imageCapture = imageCapture ?: return
+        //이미지를 보관할 MediaStore 콘텐츠 값
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply{
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+        //파일과 메타데이터를 포함한 output option 객체 생성
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        imageCapture?.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc:ImageCaptureException){
+                    Log.v(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults) {
+                    Log.v(TAG,"Photo : ${output.savedUri}")
+                    Toast.makeText(baseContext, "Photo : ${output.savedUri}", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+        )
+
+
+
+
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
+    }
+
 }
