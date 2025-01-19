44257b84b17eca7c25b397b0919a7b8cca2d4c4e
ShinYou-bin <sinddol07@naver.com>
2025-01-19 17:32:26 +0900

    Feat: splash 페이지 추가

---
+++ app/src/main/java/com/example/moneychanger/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.moneychanger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 스플래시 화면 표시 후 메인 액티비티로 전환
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // 스플래시 액티비티를 종료
+        }, 3000) // 3000ms = 3초
+    }
+}
