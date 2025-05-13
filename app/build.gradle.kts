a44a70714b0f0c04cc6a7403b33d8394c177c601
ShinYoo-bin <sinddol07@naver.com>
2025-05-13 16:59:45 +0900

    Feat: 대시보드 화면 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    implementation("io.insert-koin:koin-android:3.5.0")
+
+
     //Recognize Latin/Korean script
     implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation("com.google.mlkit:text-recognition-korean:16.0.1")
